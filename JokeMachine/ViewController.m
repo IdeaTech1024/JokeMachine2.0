4989217bc8fa13716a99d62ce3efea70f1b667dd
tbcforever <haofeng@haofengdeMacBook-Air.local>
2015-06-28 17:19:54 +0800

    hello world

--- JokeMachine/ViewController.m
+++ JokeMachine/ViewController.m
@@ -19,6 +19,8 @@
     // Do any additional setup after loading the view, typically from a nib.
 
     self.view.backgroundColor = [UIColor grayColor];
+    
+    NSLog(@"hello world");
 }
 
 - (void)didReceiveMemoryWarning {
