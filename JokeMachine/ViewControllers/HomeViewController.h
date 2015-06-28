fb7f4d0b9365a6a4bab15b358c59f2289be2143b
李永亮 <lazy10240@126.com>
2015-06-28 15:47:49 +0800

    scroll

---
+++ JokeMachine/ViewControllers/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  JokeMachine
+//
+//  Created by 李永亮 on 15/6/28.
+//  Copyright (c) 2015年 李永亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
---
+++ JokeMachine/ViewControllers/HomeViewController.m
@@ -0,0 +1,47 @@
+//
+//  HomeViewController.m
+//  JokeMachine
+//
+//  Created by 李永亮 on 15/6/28.
+//  Copyright (c) 2015年 李永亮. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *viewWidth;
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#if 0
+
+- (void)updateViewConstraints
+{
+    [super updateViewConstraints];
+    self.viewWidth.constant = CGRectGetWidth([UIScreen mainScreen].bounds)*2;
+}
+#endif
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
